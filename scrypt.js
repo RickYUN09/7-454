5401bef6f705805afcbf11765069f4a1b08352fc
RickYUN09 <aoshakba@mail.ru>
2025-05-07 00:20:09 +0500

    scrypt.js

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+function showCard() {
+    document.getElementById("card").style.display = "block";
+  }
+  
+  function showInvitation() {
+    document.getElementById("invitation").style.display = "block";
+    document.getElementById("photos").scrollIntoView({ behavior: "smooth" });
+  }
